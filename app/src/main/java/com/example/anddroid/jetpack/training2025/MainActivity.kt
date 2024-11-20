621189c7ff272201862d9fc5836d72b9bcbb52f7
kenji.tazawa <kenji.tazawa@asoview.co.jp>
2024-11-20 16:08:04 +0900

    タブの表示を追加

--- app/src/main/java/com/example/anddroid/jetpack/training2025/MainActivity.kt
+++ app/src/main/java/com/example/anddroid/jetpack/training2025/MainActivity.kt
@@ -19,17 +19,26 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Devices.PIXEL_FOLD
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,6 +62,7 @@ class MainActivity : ComponentActivity() {
                             .fillMaxSize()
                     ) {
                         SearchBox()
+                        PersonaTab()
                     }
                 }
             }
@@ -98,6 +108,33 @@ fun SearchBox() {
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PersonaTab() {
+    var state by remember { mutableIntStateOf(0) }
+    val titles = listOf("Tab 1", "Tab 2", "Tab 3")
+
+    Column {
+        PrimaryTabRow(selectedTabIndex = state) {
+            titles.forEachIndexed { index, title ->
+                Tab(
+                    selected = state == index,
+                    onClick = { state = index },
+                    text = {
+                        Text(text = title, maxLines = 2, overflow = TextOverflow.Ellipsis)
+                    }
+                )
+            }
+        }
+        Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = "Primary tab ${state + 1} selected",
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
